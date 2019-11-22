f64ae9f43d9a63984c1b108b31bb17b81d736eaa
ef69036 <emiliano.fagundes@southsystem.com.br>
2019-11-22 09:16:34 -0300

    init

---
+++ src/main/java/br/com/atox/poccamundapeoplems/PocCamundaPeopleMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.atox.poccamundapeoplems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCamundaPeopleMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCamundaPeopleMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atox/poccamundapeoplems/PocCamundaPeopleMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atox.poccamundapeoplems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocCamundaPeopleMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
